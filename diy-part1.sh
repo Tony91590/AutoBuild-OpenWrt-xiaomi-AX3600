ae3e68f96114bd4a6106f8a6365528f2f931099f
klsls <castlevania21@gmail.com>
2022-04-23 10:00:29 +0800

    revert edbd8d2e9

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,19 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'revert commit edbd8d2e9 (ipq807x: drop all xiaomi devices support)'
+git revert -n edbd8d2e9839357f3a4f0a06174d243f362b1544
+cat target/linux/ipq807x/patches-5.10/900-arm64-dts-add-OpenWrt-DTS-files.patch
+sed -i '15d;17,19d' target/linux/ipq807x/patches-5.10/900-arm64-dts-add-OpenWrt-DTS-files.patch
+cat target/linux/ipq807x/patches-5.10/900-arm64-dts-add-OpenWrt-DTS-files.patch
+sed -i 's/-3,6 +3,9/-3,6 +3,11/g' target/linux/ipq807x/patches-5.10/900-arm64-dts-add-OpenWrt-DTS-files.patch
+cat target/linux/ipq807x/patches-5.10/900-arm64-dts-add-OpenWrt-DTS-files.patch
+echo 'support xiaomi ipq8071 again'
+
+echo '添加jerrykuku的argon-mod主题'
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+echo '=========Add argon-mod OK!========='
